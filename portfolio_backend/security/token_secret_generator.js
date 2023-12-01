09b53d6c28a1eb3d73f43cac5e06a8aa822068d2
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2023-11-30 19:03:02 -0500

    Had to go back and fix something

--- portfolio_backend/security/token_secret_generator.js
+++
@@ -1,2 +0,0 @@
-// Run this in cmd with node and then get the output to get a token
-console.log(require('crypto').randomBytes(64).toString('hex'))
--- portfolio_backend/security/tokens.js
+++
@@ -1,7 +0,0 @@
-// Import jwt as jsonwebtoken
-
-function checkIfAdminToken(token) {
-	return token == process.env.ADMIN_TOKEN_SECRET;
-}
-
-export default checkIfAdminToken;
