3f4d5f156774a06982a9659f558d94a60bbce4f6
Aditya Rao <adityarao2005@gmail.com>
2023-12-08 14:10:59 -0500

    Fix layout for internal access

--- portfoliofrontend/src/components/Layout/Layout.jsx
+++ portfoliofrontend/src/components/Layout/Layout.jsx
@@ -5,20 +5,22 @@ import { Outlet } from "react-router-dom";
 function Layout() {
 	return (
 		<>
-			<NavComponent
-				links={[
-					{ id: "1", text: "Home", href: "/" },
-					{ id: "2", text: "About", href: "/about" },
-					{ id: "3", text: "Projects", href: "/projects" },
-					{ id: "4", text: "Vision", href: "/vision" },
-					{ id: "5", text: "Artwork", href: "/artwork" },
-					{ id: "6", text: "Contact", href: "/contact" },
-					{ id: "7", text: "Components", href: "/components" },
-				]}
-				brandText={<b>Aditya Rao</b>}
-			/>
+			<div class='w-screen h-screen flex flex-col'>
+				<NavComponent
+					links={[
+						{ id: "1", text: "Home", href: "/" },
+						{ id: "2", text: "About", href: "/about" },
+						{ id: "3", text: "Projects", href: "/projects" },
+						{ id: "4", text: "Vision", href: "/vision" },
+						{ id: "5", text: "Artwork", href: "/artwork" },
+						{ id: "6", text: "Contact", href: "/contact" },
+						{ id: "7", text: "Components", href: "/components" },
+					]}
+					brandText={<b>Aditya Rao</b>}
+				/>
 
-			<Outlet />
+				<Outlet />
+			</div>
 		</>
 	);
 }
